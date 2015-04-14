5a34a6f297d67679213c09264c2b722a00edc48f
myun2 <myun2@nwhite.info>
2015-04-14 15:47:33 +0900

    parser add

--- lexer.cpp
+++ lexer.cpp
@@ -1,4 +1,6 @@
 #include "lexer.hpp"
+#include "parser.hpp"
+#include <stdio.h>
 
 using namespace myun2::lexical;
 
@@ -6,5 +8,7 @@ typedef sequence<char_<'A'>, char_<'B'>, char_<'C'> > ABC;
 
 int main()
 {
+	printf("%d\n", parse<bool, ABC>("ABC"));
+	printf("%d\n", parse<bool, ABC>("AFZ"));
 	return 0;
 }
---
+++ parser.hpp
@@ -0,0 +1,16 @@
+#ifndef __MYUN2__LEXICAL__PARSER__HPP__
+#define __MYUN2__LEXICAL__PARSER__HPP__
+
+namespace myun2
+{
+	namespace lexical
+	{
+		template <typename _document, typename _Syntax, typename _Input>
+		_document parse(_Input p) {
+			while(p++) {
+			}
+		};
+	}
+}
+
+#endif//__MYUN2__LEXICAL__PARSER__HPP__
